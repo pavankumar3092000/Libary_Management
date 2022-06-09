25e4d5e0728e91857c58a5cbc1ad5153466e5b65
Pavan Kumar <69464116+pavankumar3092000@users.noreply.github.com>
2022-06-09 10:13:46 +0530

    Update urls.py

--- Libary_Management/urls.py
+++ Libary_Management/urls.py
@@ -4,5 +4,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('authenticate.urls')),
-    path('', include('products.urls')),
+    path('#/', include('products.urls')),
 ]
